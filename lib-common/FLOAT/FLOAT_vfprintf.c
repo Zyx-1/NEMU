24169997805f2e04ded1d2af7fe0f6bf8cacb743
tracer-ics2016 <tracer@njuics.org>
2020-11-08 03:28:33 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:28:33 up 33 min,  1 user,  load average: 0.76, 0.77, 0.86 fd857d23df83d65bec4fe092299c2ec3747b9bb3

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -35,19 +35,19 @@ __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f) {
 }
 
 static void modify_vfprintf() {
-	int addr = &_vfprintf_internal;	
+	int addr = &_vfprintf_internal;	//begin
 
-	mprotect((void *)((addr + 0x306 - 0x64) & 0xfffff000), 4096 * 2, PROT_READ | PROT_WRITE | PROT_EXEC);
+	//mprotect((void *)((addr + 0x306 - 0x64) & 0xfffff000), 4096 * 2, PROT_READ | PROT_WRITE | PROT_EXEC);
 
-	
-	char *sub = (char *)(addr + 0x306 - 0xb);	
-	*sub = 0x8;		
+	// guess what?
+	char *sub = (char *)(addr + 0x306 - 0xb);	//guess what?
+	*sub = 0x8;		//guess what?
 	sub = (char *)(addr + 0x306 - 0xa);
-	*sub = 0xff;	
+	*sub = 0xff;	//guess what?
 	sub = (char *)(addr + 0x306 - 0x9);
-	*sub = 0x32;	
+	*sub = 0x32;	//guess what?
 	sub = (char *)(addr + 0x306 - 0x8);
-	*sub = 0x90;	
+	*sub = 0x90;	//guess what?
 
 	sub = (char *)(addr + 0x306 - 30);
 	*sub = 0x90;
@@ -111,7 +111,7 @@ static void modify_vfprintf() {
 static void modify_ppfs_setargs() {
 	int addr = &_ppfs_setargs;
 
-	mprotect((void *)((addr + 0x73 - 0x64) & 0xfffff000), 4096 * 2, PROT_READ | PROT_WRITE | PROT_EXEC);
+	//mprotect((void *)((addr + 0x73 - 0x64) & 0xfffff000), 4096 * 2, PROT_READ | PROT_WRITE | PROT_EXEC);
 
 	char *pos = (char *)(addr + 0x71);
 	*pos = 0xeb;
@@ -119,6 +119,7 @@ static void modify_ppfs_setargs() {
 	*pos = 0x30;
 	pos = (char *)(addr + 0x73);
 	*pos = 0x90;
+	//guess guess guess what???
 
 	
 	/* TODO: Implement this function to modify the action of preparing
